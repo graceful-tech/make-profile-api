c39aae9b669cca720bbc0bfdb03ef2142af172f1
Shazia <shazia@gracefultechnologies.com>
2025-02-21 18:10:48 +0530

    Initial commit

---
+++ src/main/java/com/make_profile/MakeProfileApplication.java
@@ -0,0 +1,13 @@
+package com.make_profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/make_profile/MakeProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.make_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
